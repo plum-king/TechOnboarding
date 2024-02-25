fe0c266cff7199a8d6423b0fb4ce399e1d48be2a
Jisoo Hwang <jeesoo0908@gmail.com>
2024-02-25 16:37:56 +0000

    Update App & Home

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Jisoo Hwang",
+  title: "DevOps Engineer",
+  email: "jeesoo0908@gmail.com",
+  gitHub: "https://github.com/plum-king",
+  instagram: "https://www.instagram.com/at__plum/",
+  linkedIn: "https://www.linkedin.com/in/jisoo-hwang/",
 };
 
 const primaryColor = "#4E567E";
